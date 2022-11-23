8f37b02d5ab458f5fb367cd1cfd5f7c688f65792
surabhisanjan <surabhisanjan@gmail.com>
2022-11-23 16:30:37 +0530

    Initialization: welcome to the Gambeling Game

---
+++ src/com/bridgelanz/gambling/Gambling.java
@@ -0,0 +1,8 @@
+package com.bridgelanz.gambling;
+
+public class Gambling {
+
+    public static void main(String[] args) {
+        System.out.println("!!!Welcome to the Gambling Game!!!");
+    }
+}
