b8f4f14deeca74a1aabe84b69ae26b6d247b0fb9
surabhisanjan <surabhisanjan@gmail.com>
2022-11-23 16:43:31 +0530

    Addition: Playing the first bet

--- src/com/bridgelanz/gambling/Gambling.java
+++ src/com/bridgelanz/gambling/Gambling.java
@@ -1,10 +1,26 @@
 package com.bridgelanz.gambling;
-
+import java.util.Random;
 public class Gambling {
     static int Stake = 100;
     static int Bet = 1;
-    int day =1;
+
     public static void main(String[] args) {
+        int day =1;
         System.out.println("!!!Welcome to the Gambling Game!!!");
+        int result;
+        Random r = new Random();
+        int rand = r.nextInt(2);
+
+        System.out.println("You currently have: $"+Stake);
+        if (rand == 1){
+            System.out.println("You Won the Game");
+            result = Stake + Bet;
+        }
+        else {
+            System.out.println("You Lost the Game");
+            result = Stake - Bet;
+        }
+        System.out.println("Your have: $"+result + " now !!");
     }
-}
+    }
+
